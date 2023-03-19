77d451e16608a253b9824a47d1fcdbd0dd75e584
Monica Banda <monicabanda@Monicas-Air.lan>
2023-03-19 11:42:01 -0600

    Added Home and Contact to Navbar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -9,21 +9,37 @@ const Navbar = () => {
   const navigate = useNavigate();
   return (
     <div className="navBar">
-      <ul>
-        <li className="brand">
-          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>React/Django JWT</b>
-          </Link>
-        </li>
-        <li>
-          {user ? (
-            <button onClick={logoutUser}>Logout</button>
-          ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
-          )}
-        </li>
-      </ul>
-    </div>
+      <div className="brand">
+        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+          <b>Just For Kicks</b>
+        </Link>
+      </div>
+      <div className="middleLinks">
+        <ul>
+          <li>
+            <Link to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact">
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="rightLinks">
+        <ul> 
+          <li>
+            {user ? (
+              <button onClick={logoutUser}>Logout</button>
+            ) : (
+              <button onClick={() => navigate("/login")}>Login</button>
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>  
   );
 };
 

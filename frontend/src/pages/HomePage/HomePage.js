ad17f59037d2a107b5e9938c9f448e8b31fd5493
Monica Banda <monicabanda@Monicas-Air.lan>
2023-03-19 09:20:21 -0600

    Added Admin, Contact, Cart, and Orderhistory Pages

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -9,30 +9,30 @@ const HomePage = () => {
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [sneakers, setSneakers] = useState([]);
 
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchSneakers = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
+        let response = await axios.get("http://127.0.0.1:8000/api/sneakers/", {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        setCars(response.data);
+        setSneakers(response.data);
       } catch (error) {
         console.log(error.response.data);
       }
     };
-    fetchCars();
+    fetchSneakers();
   }, [token]);
   return (
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
+      {sneakers &&
+        sneakers.map((sneaker) => (
+          <p key={sneaker.id}>
+            {sneaker.name} {sneaker.style} {sneaker.price}
           </p>
         ))}
     </div>

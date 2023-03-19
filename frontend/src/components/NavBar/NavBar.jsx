07887a3f711fea25b51365cb6bc9c7aeec9ccb07
Monica Banda <monicabanda@Monicas-Air.lan>
2023-03-19 13:37:32 -0600

    Updated NavBar and Footer

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -3,43 +3,65 @@ import { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 import "./NavBar.css";
+import {BsCart4} from "react-icons/bs"
+
+
+const brand = (
+  <div className="brand">
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            <h2>
+            Just For Kicks
+            </h2>
+          </Link>
+        </div>
+);
+
+const cart = (
+  <span className="cart">
+              <Link to="/cart">
+                Cart
+                <BsCart4 size={20}/>
+                <p>0</p>
+              </Link>
+            </span>
+);
+
 
 const Navbar = () => {
   const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
   return (
-    <div className="navBar">
-      <div className="brand">
-        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-          <b>Just For Kicks</b>
-        </Link>
+    <navbar>
+      <div className="navBar">
+        {brand}
+
+        <nav>    
+          <ul>
+            <li>
+              <Link to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+          <div className="rightLinks">
+            <span className="links">
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/order-history">My Orders</Link>
+            </span>
+            {cart}
+          </div>
+        </nav>
+
       </div>
-      <div className="middleLinks">
-        <ul>
-          <li>
-            <Link to="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact">
-              Contact Us
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="rightLinks">
-        <ul> 
-          <li>
-            {user ? (
-              <button onClick={logoutUser}>Logout</button>
-            ) : (
-              <button onClick={() => navigate("/login")}>Login</button>
-            )}
-          </li>
-        </ul>
-      </div>
-    </div>  
+
+
+    </navbar>
   );
 };
 

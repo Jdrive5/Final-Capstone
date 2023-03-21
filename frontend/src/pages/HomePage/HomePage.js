389f64291689e5674b424bb86e6d0db478f05e38
Monica Banda <monicabanda@Monicas-Air.lan>
2023-03-20 20:53:12 -0600

    Renamed Images in Assets Folder

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,13 +1,9 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
-
 import axios from "axios";
+import "./HomePage.css";
 
 const HomePage = () => {
-  // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
-  // The "token" value is the JWT token that you will send in the header of any request requiring authentication
-  //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
   const [sneakers, setSneakers] = useState([]);
 
@@ -17,24 +13,30 @@ const HomePage = () => {
         let response = await axios.get("http://127.0.0.1:8000/api/sneakers/all/");
         setSneakers(response.data);
       } catch (error) {
-        console.log(error.response.data)
-        
+        console.log(error.response.data);
       }
     };
     fetchSneakers();
-    }, []);
-  
+  }, []);
+
   return (
-    <div className="container">
+    <div className="sneakers-container">
       <h1>Home Page!</h1>
-      {sneakers &&
-        sneakers.map((sneaker) => (
-          <p key={sneaker.id}>
-            {sneaker.name} {sneaker.style} {sneaker.price}
-          </p>
-        ))}
+      <div className="sneakers-grid">
+        {sneakers &&
+          sneakers.map((sneaker) => (
+            <div className="sneaker-item" key={sneaker.id}>
+              <img src={`/assets/${sneaker.name.toLowerCase()}.jpg`} alt={sneaker.name} />
+              <p>{sneaker.name}</p>
+              <p>{sneaker.style}</p>
+              <p>${sneaker.price}</p>
+              <button>Add to Cart</button>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
 
 export default HomePage;
+

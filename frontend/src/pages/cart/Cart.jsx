a96b29b17a0b472abb752be7b04abe3ed40b77f8
Monica Banda <monicabanda@Monicas-Air.lan>
2023-03-24 23:27:09 -0600

    Updated and Finished Cart Page and Styled NavBar and Footer

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -1,9 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import CartContext from "../../context/CartContext";
 import "./Cart.css";
+import { BsCart4 } from "react-icons/bs";
+import axios from "axios";
 
 const Cart = () => {
   const { cart, increaseQuantity, decreaseQuantity, removeFromCart } = useContext(CartContext);
+  const [cartItems, setCartItems] = useState(0);
 
   const calculateTotal = () => {
     let total = 0;
@@ -13,8 +17,25 @@ const Cart = () => {
     return total.toFixed(2);
   };
 
+  useEffect(() => {
+    const newCartItems = cart.reduce((total, item) => total + item.quanity, 0);
+    setCartItems(newCartItems);
+  }, [cart]);
+
+  const handleOrder = () => {
+    const stripeLink = "https://buy.stripe.com/test_aEU9BNcCv6cT5QQ6oo";
+    window.open(stripeLink, "_blank");
+  }
+
   return (
     <div className="cart">
+      <span className="cart">
+        <Link to="/cart">
+          Cart
+          <BsCart4 size={20}/>
+          <p>{cartItems}</p>
+        </Link>
+      </span>
       <h2>Your Cart</h2>
       <div className="cart-items">
         {cart.length > 0 ? (
@@ -37,6 +58,7 @@ const Cart = () => {
       </div>
       <div className="cart-total">
         <h3>Total: ${calculateTotal()}</h3>
+        <button onClick={handleOrder}>Order</button>
       </div>
     </div>
   );
